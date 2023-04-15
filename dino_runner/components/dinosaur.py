ebaad5b429f257539b33331f3a989119dc7bd5e8
MARCIAL-VP4 <marcial.v.pontes@gmail.com>
2023-04-14 22:12:09 -0300

    Aula2_obstaculos

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,13 +1,14 @@
 import pygame
+from pygame.sprite import Sprite
 
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
-Y_POS_DUCK = 340
+DUNK_DOWN = 340
 JUMP_VEL = 8.5
 
-class Dinosaur:
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -31,7 +32,7 @@ class Dinosaur:
             self.dino_jump = True
             self.dino_run = False
             self.dino_duck = False
-        elif not self.dino_jump:
+        elif not (self.dino_jump and self.dino_duck):
             self.dino_jump = False
             self.dino_run = True
             self.dino_duck = False
@@ -65,7 +66,7 @@ class Dinosaur:
         self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
-        self.dino_rect.y = Y_POS_DUCK
+        self.dino_rect.y = DUNK_DOWN
         self.step_index += 1
 
 
